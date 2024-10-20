20cdd2c7c977196a4f01593454ad5d2b02991731
Kadu <carloseduardolbraz@gmail.com>
2024-10-20 14:58:03 -0300

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+exams = [
+  {:name=>"RT-PCR para SARS-CoV-2 (Covid-19)", :tuss=>"40314618"},
+  {:name=>"Sorologia para COVID-19 (IgM e IgG anti-S-RBD)", :tuss=>"40324778"},
+  {:name=>"SCID - Teste do Pezinho", :tuss=>"40503143"},
+  {:name=>"MCAD - Teste do Pezinho", :tuss=>"40321029"},
+  {:name=>"Doença de Pompe - Teste do Pezinho", :tuss=>nil},
+  {:name=>"Tandem - Teste do Pezinho Qualitativo", :tuss=>"40312224"},
+  {:name=>"PKU Fenilalanina - Teste do Pezinho", :tuss=>"40301818"},
+  {:name=>"Cromatografia de Aminoácidos - Teste do Pezinho", :tuss=>"40301672"},
+  {:name=>"Hemoglobinopatias - Teste do Pezinho", :tuss=>"40319474"},
+  {:name=>"TSH - Teste do Pezinho", :tuss=>"40316521"},
+  {:name=>"T4 - Teste do Pezinho", :tuss=>"40316548"},
+  {:name=>"17 OH Progesterona - Teste do Pezinho", :tuss=>"40316017"},
+  {:name=>"Tripsina Imuno Reativa - Teste do Pezinho", :tuss=>"40302563"},
+  {:name=>"Biotinidase - Teste do Pezinho", :tuss=>"40306488"},
+  {:name=>"Toxoplasmose IgM - Teste do Pezinho", :tuss=>"40307832"},
+  {:name=>"Galactose Total - Teste do Pezinho", :tuss=>"40301974"},
+  {:name=>"G6PD - Teste do Pezinho", :tuss=>"40302059"},
+  {:name=>"Citomegalovirus IgM - Teste do Pezinho", :tuss=>"40306674"},
+  {:name=>"Treponema Pallidum IgM - Teste do Pezinho", :tuss=>"40306615"},
+  {:name=>"CHAGAS NEONATAL - ANTICORPOS TOTAIS - Teste do pezinho", :tuss=>"40306615"}
+]
+
+
+exams.each do |exam_hash|
+  next if Catalog::Exam.exists?(exam_hash)
+
+  Catalog::Exam.create(exam_hash)
+end
+
+puts 'Exames cadastrados com sucesso!'
